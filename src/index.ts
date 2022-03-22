2445e7728580de38912c22d18f4b1e770d4ca061
NehuenLom <93491147+Nehuenlom@users.noreply.github.com>
2022-03-22 14:26:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+let nombreComercio: string;
+nombreComercio = "Mercado El Irlandes Medieval";
+console.log(nombreComercio);
+
+let primerProducto: string;
+let cantidadUnidades: number;
+let precioProducto: number;
+primerProducto = "YERBA MATE AMANDA";
+cantidadUnidades = 70;
+precioProducto = 120;
+console.log(primerProducto","cantidadUnidades "Unidades,""$"precioProducto);
+
+let compre: number;
+let stock: number;
+let ganancia: number;
+compre = 4;
+stock = cantidadUnidades - compre;
+ganancia = precioProducto * compre;
+console.log("compre"compre,",saque ganancia $"ganancia,",me quedan",stock"Unidades");
+
+
+
+
+
+
+
+
+
+console.log("AZUCAR LEDEZMA, 300 unidades");
+console.log("FERNET BRANCA, 241 unidades");
+console.log("COCA-COLA, 300 unidades");
+console.log("FIDEOS LARGOS, 500 unidades");
+console.log("ARROZ MAROLIO, 427 unidades");
+console.log("DESODORANTE AXE, 220 unidades");
+console.log("LAVANDINA AYUDIN, 400 unidades");
+console.log("LECHE LARGA VIDA, 500 unidades");
---
+++ src/index4.ts
@@ -0,0 +1,16 @@
+let numero_1: number = 8; // inicializado
+let numero_2: number = 5;
+let resultado: number = numero_1 + numero_2;
+
+console.log("Primer resultado:", resultado);
+
+let numero_1: number; // no inicializado
+numero_1 = 91;
+resultado = numero_1 + numero_2;
+
+console.log("Segundo resultado:", resultado);
+
+let num: number = 5;
+let num2: number = 8;
+let result: number = 3;
+console.log(result === num + num2);
